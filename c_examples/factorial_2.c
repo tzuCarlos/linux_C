1c0a5a16fa98e0cf0736cfef2e4e71821dc8d069
karose.tzu@gmail.com <karose.tzu@gmail.com>
2014-06-26 16:48:43 +0800

    add big number factorial example

--- c_examples/factorial_2.c
+++ c_examples/factorial_2.c
@@ -1,23 +1,14 @@
 #include<stdio.h>
 #include<math.h>
+#include <stdlib.h>
+
 /*
 *  example for recursion and iteration 
 *  I know it should return error if n < 0, but this just a simple example for study.
 *  Do not care about overflow issues this example. 
 */
-unsigned int factorial_recursion(int n)
-{
-
-   /* return n>=1 ? n * factorial_recursion(n-1) : 1; */
-
-   if(n==0)
-      return 1;  /*Base case */
-   else
-      return n * factorial_recursion(n-1); /*General case */
-
-}
-
-int * toArray(int number)
+#define SIZE_A 30
+int* toArray(int number)
 {
     int n = log10(number) + 1;
     int i;
@@ -28,6 +19,7 @@ int * toArray(int number)
     }
     return numberArray;
 }
+
 int int_to_array(int num, int *array,int len)
 {
   int i;
@@ -42,25 +34,26 @@ for (i = (len-1); i >0 ; i--) {
 }
 
 }
+
+unsigned int factorial_recursion(int n)
+{
+
+   /* return n>=1 ? n * factorial_recursion(n-1) : 1; */
+
+   if(n==0)
+      return 1;  /*Base case */
+   else
+      return n * factorial_recursion(n-1); /*General case */
+
+}
+
 unsigned int factorial_iteration(int n)
 {
   int i=1;
   int result=1;
 
   int a[10]= {0};
-  int *p;
 
-  int_to_array(n, a,10);
-  p=toArray(n);
-
-  for(i=0 ; i <10; i++)
-  printf("%d",a[i]);
-  printf("........\n");
-
-  for(i=0 ; i <10; i++)
-  printf("%d",*(p+i));
-  
-  return 1;
   for(i=1; i <= n  ; i++)
   {
     /*result *= i */
@@ -70,10 +63,45 @@ unsigned int factorial_iteration(int n)
   return result;
 }
 
+void Big_factorial_iteration(int n)
+{
+  int i=1;
+
+  /*init array*/
+  int a[SIZE_A]= {0};
+  int tail=SIZE_A-1;
+  
+  a[tail]=1;
+
+  for(i=1; i <= n  ; i++)
+  {
+    /*result *= i */
+    int j;
+    for(j= tail ; j >= 0 ; j--)
+    {
+      a[j]=a[j]*i;
+    }
+
+    for(j= tail ; j >= 0 ; j--)
+    {
+      if (a[j] >= 10)
+          a[j-1]=a[j-1]+(a[j]/10);
+          a[j]=a[j]%10;
+    }
+  }
+
+  for(i=0; i< SIZE_A ; i++)
+  {
+     printf("%d",a[i]);
+  }
+  printf("\n");
+
+}
+
 int main()
 {
 
   printf(" %d \n",factorial_recursion(10));
-  printf(" %d \n",factorial_iteration(123456789));
-
+  printf(" %d \n",factorial_iteration(10));
+  Big_factorial_iteration(20);
 }
